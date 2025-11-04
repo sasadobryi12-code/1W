82951cd51d93fba090c3178eef7e4a0bffb43c24
sasadobryi12-code <sasadobryi12@gmail.com>
2025-11-04 16:29:51 +0300

    Update Python_7.py

--- Python_7.py
+++ Python_7.py
@@ -1,5 +1,11 @@
 # Задание 1 ---------------------------------------------
 
+import os
+import re
+import inspect
+import numpy as np
+from collections import Counter
+
 def task_01_func(year):
     if year % 4 == 0:
         if year % 100 != 0:
@@ -52,7 +58,7 @@ print(p_factorial_sum)
 #  Второе решение
 
 def task_03_func(n):
-    s, p = 0, 15
+    s, p = 0, 1
     for i in range(1, n+1):
         p *= i
         s += p
@@ -81,8 +87,8 @@ print(sum_factorial(4))
 # Решение 1 
 
 def task_04_func(s):
-    n_r = ''.join(reversed(n))
-    if n_r == n:
+    n_r = ''.join(reversed(s))
+    if n_r == s:
         return 'Слово является полиндромом'
     else:
         return 'Слово не является полиндромом'
@@ -96,7 +102,8 @@ def task_041_func(s):
     s_lower = s.lower()
     return s_lower == s_lower[::-1]
 
-n = str(input())s = input()
+n = str(input())
+s = input()
 polindrom = True
 dlin = len(s)
 for i in range(dlin//2):
@@ -140,8 +147,6 @@ word_input = input("Введите текст: ")
 print(task_05_func(word_input))
 
 #  Решение 2
-
-from collections import Counter
 
 word_input = input("Введите текст: ")
 print(dict(Counter((word_input.split(' ')))))
@@ -188,8 +193,6 @@ print(struc(a))
 
 # Решение 2
 
-import numpy as np
-
 def sitruc1(n):
     b = np.copy(n).tolist() 
     
@@ -218,6 +221,24 @@ def stucture(n):
 a = [1, 2, -3, 4, 5, -1, -2, 3, -4, -5]
 print(stucture(a))
 
+# Задание 8 ---------------------------------------------
+
+def task_08_func(lst, index):
+    return (t for t in sorted(lst, key=lambda x: x[index], reverse=True))
+
+print(list(task_08_func([(1, 5), (2, 3), (0, 10)], 1)))  # [(0, 10), (1, 5), (2, 3)]
+
+# Задание 9 ---------------------------------------------
+
+def task_09_func(n):
+    row = [1]
+    for _ in range(n):
+        print(row)
+        row = [1] + [row[i] + row[i+1] for i in range(len(row)-1)] + [1] if row else [1]
+
+task_09_func(5)
+
+
 # Задание 11 ---------------------------------------------
 
 def task_11_func(first_list, second_list):
@@ -229,3 +250,83 @@ print(task_11_func([1, 2, 2, 3, 4], [2, 4]))  # [1, 3]
 print(task_11_func([5, 6, 7], [1, 2]))        # [5, 6, 7]
 print(task_11_func([1, 1, 1], [1]))           # []
 print(task_11_func([3, 1, 3, 2], [3]))        # [1, 2]
+
+# Задание 13 ---------------------------------------------
+
+def task_13_func(n):
+    def gen(limit):
+        a, b = 0, 1
+        i = 0
+        while limit == -1 or i < limit:
+            yield a
+            a, b = b, a + b
+            i += 1
+    return gen(n)
+
+print(list(x for x in task_13_func(7)))  # 0..
+# Бесконечный использовать осторожно: next(task_13_func(-1))
+
+# Задание 14 ---------------------------------------------
+
+def task_14_func(obj):
+    attrs = dir(obj)
+    if isinstance(obj, int):
+        return list(filter(lambda m: m.startswith('__a'), attrs))
+    if isinstance(obj, str):
+        return list(filter(lambda m: m.startswith('__s'), attrs))
+    return list(filter(lambda m: not (m.startswith('__') and m.endswith('__')), attrs))
+
+print(task_14_func(1)[:3])
+print(task_14_func('x')[:3])
+print(task_14_func([])[:5])
+
+# Задание 15 ---------------------------------------------
+
+# Решение 1 (без циклов)
+def task_15_func_no_loops(s):
+    replaced = re.sub(r'[A-Z]', lambda m: str(ord(m.group(0))), s)
+    digits = re.findall(r'\d', replaced)
+    split_digit = min(digits) if digits else '0'
+    return replaced.split(split_digit)
+
+# Решение 2 (до двух проходов)
+def task_15_func_loops(s):
+    parts = []
+    current = ''
+    replaced = ''
+    for ch in s:
+        if 'A' <= ch <= 'Z':
+            replaced += str(ord(ch))
+        else:
+            replaced += ch
+    min_digit = '9'
+    for ch in replaced:
+        if ch.isdigit() and ch < min_digit:
+            min_digit = ch
+    for ch in replaced:
+        if ch == min_digit:
+            parts.append(current)
+            current = ''
+        else:
+            current += ch
+    parts.append(current)
+    return parts
+
+print(task_15_func_no_loops('AbC9'))
+print(task_15_func_loops('AbC9'))
+
+# Задание 17 ---------------------------------------------
+
+# Решение 1 (рекурсивное)
+def task_17_func_recursive(tree):
+    def dfs(node, acc):
+        if node is None:
+            return
+        val, left, right = node
+        acc += val
+        if left is None and right is None:
+            print(acc)
+            return
+        dfs(left, acc)
+        dfs(right, acc)
+    dfs(tree, 0)
