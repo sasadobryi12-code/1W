1a1010ef8b14998dbc869a3477c5ae89934e7352
sasadobryi12-code <sasadobryi12@gmail.com>
2025-12-07 16:51:40 +0300

    Create turtle.py

---
+++ turtle.py
@@ -0,0 +1,23 @@
+import turtle
+
+screen = turtle.Screen()
+screen.bgcolor("white")
+screen.setup(800, 800)
+screen.title("Большая спираль с множеством линий")
+
+t = turtle.Turtle()
+t.color("black")
+t.pensize(1)
+
+angle = 10
+step = 0.3
+length = 2
+loops = 600
+
+for i in range(loops):
+    t.forward(length)
+    t.right(angle)
+    length += step
+
+
+turtle.done()
